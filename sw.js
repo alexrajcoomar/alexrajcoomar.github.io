b78b1aefa212354d05c39e3bc0f99b3407959d76
site build <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-04 17:33:32 +0000

    Rebuild site [skip ci]

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    this worker's to delete. */
 const VERSION  = "0545d2eb1dc4";
 const CORE     = "site-" + VERSION;
-const PAGES    = "site-pages-ba78b290b169";
+const PAGES    = "site-pages-fff0bcb312af";
 const MANIFEST = "offline-manifest.json";
 const FILES    = [
   "daily-learning-cockpit-192.png",
